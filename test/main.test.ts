f26510ecebc591c325fd9651f28e9c692d2ace66
DARSAN <iamspdarsan@gmail.com>
2024-11-29 10:51:25 +0530

    test case: sitemap validator - added

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,31 +1,11 @@
-import { readFileSync } from "fs";
-import { join } from "path";
-import { validateXML } from "xsd-schema-validator";
-import config from "../configLoader";
-import { makeSitemap } from "../lib/utils";
+import { Hawk } from "../lib/core";
+import validateSitemap from "./utils/validate-sitemap";
 
-async function _validateSitemap(): Promise<boolean> {
-	//Generate site map
-	await makeSitemap(true, [], [], true);
-
-	/* Loading sitemap.xml */
-	const sitemapPath: string = config.sitemapPath;
-	const sitemapXML: string = readFileSync(sitemapPath, {
-		encoding: "utf8",
-	});
-
-	const sitemapSchemaFile: string = join(__dirname, "sitemap-schema.xsd");
-
-	/* Validating */
-	try {
-		const result = await validateXML(sitemapXML, sitemapSchemaFile);
-		return result.valid;
-	} catch (err) {
-		console.log(err);
-		return false;
-	}
-}
+const hawkInstance = new Hawk();
+const testSampleRootPath = "./test/test-sample";
 
 test("Sitemap.xml validation", async () => {
-	expect(await _validateSitemap()).toBe(true);
+	expect(await validateSitemap(testSampleRootPath, hawkInstance)).toBe(
+		true,
+	);
 });
---
+++ test/utils/validate-sitemap.ts
@@ -0,0 +1,87 @@
+import { XMLParser } from "fast-xml-parser";
+import { globSync } from "glob";
+import { existsSync, readFileSync, rmSync } from "node:fs";
+import { join } from "node:path";
+import { validateXML } from "xsd-schema-validator";
+import { type Hawk } from "../../lib/core";
+
+export default async function validateSitemap(
+	testSampleRootPath: string,
+	hawkInstance: Hawk,
+): Promise<boolean> {
+	process.chdir(testSampleRootPath);
+
+	const lookupPattern = ["**/*.html"];
+	const uploadToFTP = false;
+	const expectedSitemapOutputPath = "test-sitemap.xml";
+
+	hawkInstance.configurations.sitemapPath = expectedSitemapOutputPath;
+	await hawkInstance.utils.makeSitemap(
+		lookupPattern,
+		[],
+		false,
+		uploadToFTP,
+	);
+
+	//check sitemap if exist
+	const siteMapExist = existsSync(expectedSitemapOutputPath);
+
+	if (siteMapExist) {
+		//validate sitemap with schema
+		const sitemapXML: string = readFileSync(expectedSitemapOutputPath, {
+			encoding: "utf8",
+		});
+
+		//delete sitemap as no longer needed
+		rmSync(expectedSitemapOutputPath, { recursive: true, force: true });
+
+		const sitemapSchemaFile = "sitemap-schema.xsd";
+
+		const { valid } = await validateXML(sitemapXML, sitemapSchemaFile);
+
+		if (valid) {
+			//check number of available routes against nof available files;
+			const parser = new XMLParser();
+			const parsed = parser.parse(sitemapXML);
+
+			const urls = parsed.urlset.url.map(
+				(url: { loc: string; lastmod: string }) => url.loc,
+			);
+			const availableRoutes = globSync(lookupPattern);
+
+			const numberOfRoutesinMap = urls.length;
+			const numberOfFiles = availableRoutes.length;
+
+			const expectedRoutesCount = numberOfFiles === numberOfRoutesinMap;
+
+			if (expectedRoutesCount) {
+				//ping to all routes if any failed return false
+				return _pingRoutes(urls);
+			}
+		} else {
+			console.log("⚠️  Sitemap failed at schematic test");
+		}
+	} else {
+		console.log("⚠️  Sitemap not found!");
+	}
+
+	return false;
+}
+
+function _pingRoutes(urls: string[]): boolean {
+	return urls.every((url: string) => {
+		let { pathname } = new URL(url);
+
+		if (pathname === "/") pathname = "index";
+
+		const filePath = join(process.cwd(), pathname + ".html");
+
+		const goodRoute = existsSync(filePath);
+
+		if (!goodRoute) {
+			console.log(`⚠️  Ping failed on: ${url}`);
+		}
+
+		return goodRoute;
+	});
+}
